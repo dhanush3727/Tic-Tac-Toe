f4cbcebe64417c3f57c77d7a337edc8c19b9b01f
dhanush3727 <dhanushs4827@gmail.com>
2024-01-05 15:46:47 +0530

    Game

---
+++ script.js
@@ -0,0 +1,53 @@
+const boxes = document.querySelectorAll(".box");
+var playerTurn, moves, restartGame;
+playerTurn = "x";
+moves = 0;
+restartGame = "<button>Restart</button>";
+
+boxes.forEach((box) => {
+  box.addEventListener("click", () => {
+    if (box.dataset.player == "none") {
+      box.innerHTML = playerTurn;
+      box.dataset.player = playerTurn;
+      moves++;
+      if (playerTurn == "x") {
+        playerTurn = "o";
+      } else if (playerTurn == "o") {
+        playerTurn = "x";
+      }
+    }
+    const checkWinner = (a, b, c) => {
+      if (
+        boxes[a].dataset.player === boxes[b].dataset.player &&
+        boxes[b].dataset.player === boxes[c].dataset.player &&
+        boxes[c].dataset.player === boxes[a].dataset.player &&
+        (boxes[a].dataset.player === "x" || boxes[a].dataset.player == "o")
+      ) {
+        const gameOver = (a) => {
+          var gameAlert =
+            "GameOver <br> Player " +
+            boxes[a].dataset.player.toUpperCase() +
+            " Win<br><br>" +
+            restartGame;
+          var div = document.createElement("div");
+          div.className = "gameOver";
+          div.innerHTML = gameAlert;
+          document.body.append(div);
+          moves = 0;
+        };
+        gameOver(a);
+      }
+    };
+    checkWinner(0, 1, 2);
+    checkWinner(3, 4, 5);
+    checkWinner(6, 7, 8);
+    checkWinner(0, 3, 6);
+    checkWinner(1, 4, 7);
+    checkWinner(2, 5, 8);
+    checkWinner(0, 4, 8);
+    checkWinner(2, 4, 6);
+    if (moves == 9) {
+      const draw = () => {};
+    }
+  });
+});
